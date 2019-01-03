4a175c0c51cd64d9da0a09fc7e8544c90a1e5c72
Sim Greenbaum <gbs4ever2@gmail.com>
2019-01-03 01:42:45 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -43,7 +43,7 @@ def get_occupation(data, hometown)
 end
 
 def get_average_age_for_season(data, season)
-Array = []
+array = []
   data[season].each do |person_data|
 answer= person_data["age"].to_f
 array <<answer
